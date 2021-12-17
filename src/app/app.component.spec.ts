22714556c2f73421dac88dd6080d0f9a8c303dd8
Roger <roger@rcod.net>
2021-12-17 11:15:22 +0100

    mediarecorder bug and update to pass tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {SensorsModule} from "../../projects/tesla-ce/sensors/src/lib/sensors.module";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        SensorsModule
       ],
       declarations: [
         AppComponent
